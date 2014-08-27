b2e22b39665683995dced92f0fbaee6d4503b51f
unknown <Student@PS458S-5KR6MVIOIN60.students.dc_schools.k12.dc.us>
2014-08-27 13:37:31 -0400

    New Lang

--- essays/createproc.php
+++ essays/createproc.php
@@ -1,7 +1,11 @@
 <?php
-$body = $_POST["body"]; 
-$file= $title.".html";
-file_put_contents($file, $_POST["body"]);
+$title = $_POST["title"];
+$body = $_POST["body"];
+$head = "<!DOCTYPE HTML><html><head><title>".$title."</title></head><body>";
+$ext = ".html";
+$name = $title;
+$file = $name.$ext;
+file_put_contents($file,$head.$_POST["body"]."</body></html>");
 
 
 ?>
